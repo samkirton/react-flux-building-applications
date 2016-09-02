60b16b24fa949f4f9ab03bf27737bf519db7c98e
samkirton <samkirton@me.com>
2016-09-02 21:20:19 +0100

    4-6 > willTransitionTo / willTransitionFrom

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -48709,6 +48709,20 @@ module.exports = {
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
+	statics: {
+		// willTransitionTo: function(transition, params, query, callback) {
+		// 	if (!confirm("Are you sure you want to read a page?")) {
+		// 		transition.abort();
+		// 	} else {
+		// 		callback();
+		// 	}
+		// },
+		willTransitionFrom: function(transition, component) {
+			if (!confirm("Are you sure you want to leave the page?")) {
+				transition.abort();
+			}
+		}
+	},
 	render: function() {
 		return (
 			React.createElement("div", null, 
--- src/components/about/aboutpage.js
+++ src/components/about/aboutpage.js
@@ -3,6 +3,20 @@
 var React = require('react');
 
 var About = React.createClass({
+	statics: {
+		// willTransitionTo: function(transition, params, query, callback) {
+		// 	if (!confirm("Are you sure you want to read a page?")) {
+		// 		transition.abort();
+		// 	} else {
+		// 		callback();
+		// 	}
+		// },
+		willTransitionFrom: function(transition, component) {
+			if (!confirm("Are you sure you want to leave the page?")) {
+				transition.abort();
+			}
+		}
+	},
 	render: function() {
 		return (
 			<div>
