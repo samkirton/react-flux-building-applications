9f00510cc055e32124c7052ae7d417e429a5aa0e
samkirton <samkirton@me.com>
2016-09-02 22:27:14 +0100

    5-8 > more prop types

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -49153,6 +49153,12 @@ var React = require('react');
 var Input = require('../common/textinput');
 
 var AuthorForm = React.createClass({displayName: "AuthorForm",
+	propTypes: {
+		author: React.PropTypes.object.isRequired,
+		onSave: React.PropTypes.func.isRequired,
+		onChange: React.PropTypes.func.isRequired,
+		errors: React.PropTypes.object
+	},
 	render: function() {
 		return (
 			React.createElement("form", null, 
